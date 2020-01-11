269bb7c512585ebfc1a111f1b6e5cbdff9108876
BIT504 <mounikakuknoor@myop.ac.nz>
2020-01-11 19:46:11 +1300

    Third line of assignment stating favourite mode of transport

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -5,6 +5,7 @@ public class PartD {
 		// TODO Auto-generated method stub
 		 System.out.println("Hi, my name is Mounika");
 		 System.out.println("My favourite colour is Sky Blue");
+		 System.out.println("My favourite mode of transportation is Aeroplane");
 	}
 
 }
