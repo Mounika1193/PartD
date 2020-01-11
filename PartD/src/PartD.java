cd3fd8190255119fb79e290b37ec821e10f63965
BIT504 <mounikakuknoor@myop.ac.nz>
2020-01-11 19:41:54 +1300

    Second line of assignment stating favourite colour

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -4,6 +4,7 @@ public class PartD {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("Hi, my name is Mounika");
+		 System.out.println("My favourite colour is Sky Blue");
 	}
 
 }
